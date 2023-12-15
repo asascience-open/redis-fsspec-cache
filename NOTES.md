b2ccb7ac978d198ee2dc9c6c8e0114482d9424af
Matthew Iannucci <mpiannucci@gmail.com>
2023-12-15 15:27:25 -0500

    Start thinkout about impl

--- NOTES.md
+++ NOTES.md
@@ -5,3 +5,7 @@ We need two pieces to get this working:
 1. A Cache implementation, derived from `BaseCache` to store and retreive chunks from redis
 2. A Filesystem implementation, derived from `BaseFilesystem` that will wrap `_open` to cache file as it is read.
 
+Questions: 
+
+1. Do we need to make a block cache at all?
+2. What about async? 
--- redis_block_cache/cache.py
+++ redis_block_cache/cache.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from fsspec.caching import BaseCache
+from redis import Redis
 
 # IDK not exported from fsspec
 # https://github.com/fsspec/filesystem_spec/blob/5df4b0b30dc011f9d6eceded5a078e92b2b5c11d/fsspec/caching.py#L36
@@ -29,24 +30,35 @@ class RedisBlockCache(BaseCache):
     name = "redisblockcache"
 
     def __init__(
-        self, blocksize: int, fetcher: Fetcher, size: int, maxblocks: int = 32
+        self,
+        blocksize: int,
+        fetcher: Fetcher,
+        size: int,
+        maxblocks: int = 32,
+        redis: Redis = None,
+        filename: str = None,
     ) -> None:
         super().__init__(blocksize, fetcher, size, maxblocks)
+        self.redis = redis
+        self.filename = filename
 
     def __repr__(self) -> str:
         return (
             f"<RedisBlockCache blocksize={self.blocksize}, "
             f"size={self.size}, nblocks={self.nblocks}>"
         )
-    
-    def _fetch(self, start: int | None, end: int | None) -> bytes:
+
+    def _fetch(self, start: int | None, stop: int | None) -> bytes:
         if start is None:
             start = 0
-        if end is None:
-            end = self.size
-        if start >= self.size or start >= end:
+        if stop is None:
+            stop = self.size
+        if start >= self.size or start >= stop:
             return b""
-        
+        return self.fetcher(start, stop)
+
         # byte position -> block numbers
-        start_block_number = start // self.blocksize
-        end_block_number = end // self.blocksize
+        _start_block_number = start // self.blocksize
+        _end_block_number = stop // self.blocksize
+
+
